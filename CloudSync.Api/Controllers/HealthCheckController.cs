221ef79bb251044bb1fca6da87c3c552a580d625
Matei Giurgiu <giurgiu.matei@yahoo.com>
2025-04-13 00:41:15 +0300

    connecting to azure works

---
+++ CloudSync.Api/Controllers/HealthCheckController.cs
@@ -0,0 +1,22 @@
+﻿using CloudSync.Data.Contexts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudSync.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController(AzureDbContext azureContext, AwsDbContext awsContext) : ControllerBase
+{
+    [HttpGet("test-connections")]
+    public async Task<IActionResult> TestConnections()
+    {
+        bool azureConnected = await azureContext.Database.CanConnectAsync();
+        bool awsConnected = await awsContext.Database.CanConnectAsync();
+
+        return Ok(new
+        {
+            AzureConnection = azureConnected,
+            AWSConnection = awsConnected
+        });
+    }
+}
